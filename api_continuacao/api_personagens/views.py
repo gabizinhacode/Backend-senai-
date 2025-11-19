be1541e35c5bfc8333e5425d448e8cbf8b3317ec
Gabriela Santos <gaabi3028@gmail.com>
2025-11-19 16:17:14 -0300

    GET e DELETE

--- api_continuacao/api_personagens/views.py
+++ api_continuacao/api_personagens/views.py
@@ -4,38 +4,121 @@ from .serializers import ObraSerializer, AtorDubladorSerializer, PersonagemSeria
 from rest_framework.response import Response
 from rest_framework import status
  
-@api_view(['GET'])
+@api_view(['GET' 'DELETE'])
 def obra_detalhe(request, id):
-    obra = Obra.objects.get(pk=id)
-    serializer = ObraSerializer(obra)
-    return Response(serializer.data)
+    try: 
+        obra = Obra.objects.get(pk=id)
+    except Obra.DoesNotExist:
+        return Response({"mensagem": "Obra não encontrada"}, status=status.HTTP_404_NOT_FOUND)  
+    
+    if request.method == 'GET':
+        serializer = ObraSerializer(obra)
+        return Response(serializer.data)
+    if request.method == 'DELETE':
+        obra.delete()
+        return Response({"mensagem": "Obra deletada!"})
+    
+  
  
-@api_view(['GET'])
+@api_view(['GET', 'DELETE'])
 def atordublador_detalhe(request, id):
-   atordublador = AtorDublador.objects.get(pk=id)
-   serializer = AtorDubladorSerializer(atordublador)
-   return Response(serializer.data)
+    try:
+        atordublador = AtorDublador.objects.get(pk=id)
+    except AtorDublador.DoesNotExist:
+        return Response({"mensagem": "Ator/Dublador não encontrado"}, status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = AtorDubladorSerializer(atordublador)
+        return Response(serializer.data)
+    if request.method == 'DELETE':
+        atordublador.delete()
+        return Response({"mensagem": "Ator/Dublador deletado!"})
+
  
-@api_view(['GET'])
+@api_view(['GET' 'DELETE'])
 def personagem_detalhe(request, id):
-   personagem = Personagem.objects.get(pk=id)
-   serializer = PersonagemSerializer(personagem)
-   return Response(serializer.data)
+    try:
+        personagem = Personagem.objects.get(pk=id)
+    except Personagem.DoesNotExist:
+        return Response({"mensagem": "Personagem não encontrado"}, status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = PersonagemSerializer(personagem)
+        return Response(serializer.data)
+    if request.method == 'DELETE':
+        personagem.delete()
+        return Response({"mensagem": "Personagem deletado!"})
+  
  
-@api_view(['GET'])
+@api_view(['GET' 'DELETE'])
 def habilidade_detalhe(request, id):
-   habilidade = Habilidade.objects.get(pk=id)
-   serializer = HabilidadeSerializer(habilidade)
-   return Response(serializer.data)
+    try:
+        habilidade = Habilidade.objects.get(pk=id)
+    except Habilidade.DoesNotExist:
+        return Response({"mensagem": "Habilidade não encontrada"}, status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = HabilidadeSerializer(habilidade)
+        return Response(serializer.data)
+    if request.method == 'DELETE':
+        habilidade.delete()
+        return Response({"mensagem": "Habilidade deletada!"})
+  
  
-@api_view(['GET'])
+@api_view(['GET' 'DELETE'])
 def relacionamento_detalhe(request, id):
-   relacionamento = Relacionamento.objects.get(pk=id)
-   serializer = RelacionamentoSerializer(relacionamento)
-   return Response(serializer.data)
+    try: 
+        relacionamento = Relacionamento.objects.get(pk=id)
+    except Relacionamento.DoesNotExist:
+        return Response({"mensagem": "Relacionamento não encontrado"}, status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = RelacionamentoSerializer(relacionamento)
+        return Response(serializer.data)
+    if request.method == 'DELETE':
+        relacionamento.delete()
+        return Response({"mensagem": "Relacionamento deletado!"})
+
+   
  
-@api_view(['GET'])
+@api_view(['GET' 'DELETE'])
 def personagemhabilidade_detalhe(request, id):
-   personagemhabilidade = PersonagemHabilidade.objects.get(pk=id)
-   serializer = PersonagemHabilidadeSerializer(personagemhabilidade)
-   return Response(serializer.data)
+    try:
+        personagemhabilidade_detalhe = PersonagemHabilidade.objects.get(pk=id)
+    except PersonagemHabilidade.DoesNotExist:
+        return Response({"mensagem": "PersonagemHabilidade não encontrada"}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = PersonagemHabilidadeSerializer(personagemhabilidade_detalhe)
+        return Response(serializer.data)  
+    if request.method == 'DELETE':
+        personagemhabilidade_detalhe.delete()
+        return Response({"mensagem": "PersonagemHabilidade deletada!"})
+
+@api_view(['DELETE'])
+def deletar_obra(request, id):
+    try:
+        obra = Obra.objects.get(pk=id)
+    except Obra.DoesNotExist:
+        return Response({"mensagem": "Obra não encontrada"})
+    
+    obra.delete()
+    return Response({"mensagem": "Obra deletada!"})
+
+
+
+@api_view(['GET''DELETE'])
+def deletar_personagem(request, id):
+    try:
+        personagem = Personagem.objects.get(pk=id)
+    except Personagem.DoesNotExist:
+        return Response({"mensagem": "Personagem não encontrado"}, status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = PersonagemSerializer(personagem)
+        return Response(serializer.data)
+    if request.method == 'DELETE':
+        personagem.delete()
+        return Response({"mensagem": "Personagem deletado!"})
+
+   
--- api_continuacao/sistema_personagens/urls.py
+++ api_continuacao/sistema_personagens/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+# Removed direct import of obra_detalhe.urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('obra_detalhe/', include('api_personagens.urls')),
 ]
